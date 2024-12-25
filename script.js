6cd5d4d5b5d03f3727551b8346e84e2ae2f72cca
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 15:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//your JS code here. If required.
-// Prompt the user to enter two numbers
-const num1 = prompt("Enter the first number:");
-const num2 = prompt("Enter the second number:");
-
-// Compare the numbers and store the result as true or false
-const isEqual = num1 === num2;
-
-// Display the result in an alert box
-alert(`Are the two numbers equal? ${isEqual}`);
+const num1 = Number(prompt("Enter the first number:"));
+const num2 = Number(prompt("Enter the second number:"));
+alert(num1 === num2);
